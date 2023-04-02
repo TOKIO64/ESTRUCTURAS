f4e2216c5a97df742a1c540cae8b379892534e9f
ᴋᴏᴀɢᴜʟᴀꜱᴏʟᴠᴇ <104767426+TOKIO64@users.noreply.github.com>
2023-04-02 21:10:55 +0200

    Create Metodos.dart

---
+++ Metodos.dart
@@ -0,0 +1,41 @@
+
+Container(
+
+    decoration : BoxDecoration(
+
+        color : Colors.grey[900]! ,
+
+        borderRadius : BorderRadius.all(),
+
+    ),
+
+),
+
+padding : EdgeInsets.only(),
+
+Row(
+
+    children : <Widget> [
+
+      Column(
+
+          children : <Widget> [],
+
+      ),
+
+    ],
+
+),
+
+SizedBox.expand(),
+
+FractionallySizedBox(
+
+    widthFactor : 0.5 ,
+
+    heightFactor : 0.5 ,
+
+    alignment: FractionalOffset.center,
+
+),
+
