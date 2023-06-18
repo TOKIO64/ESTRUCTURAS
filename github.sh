1d6878068cc40b4d6b0f14a8c1a9102a89c1c7ae
= <=>
2023-06-19 00:56:09 +0200

    okey

---
+++ github.sh
@@ -0,0 +1,14 @@
+
+	ls -al ~/.ssh
+
+	ssh-keygen -t rsa -b 4096 -C "correo_electronico@gmail.com" 
+
+	eval "$(ssh-agent -s)"
+
+	ssh-add ~/.ssh/id_rsa
+
+	cat ~/.ssh/id_rsa.pub | micro
+
+	# github > ssh key > key micro
+
+	git remote set-url origin git@github.com:nombre_usuario/nombre_repositorio.git
